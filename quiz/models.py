2168eb1ae6499afb905b170c85d073b962630423
gogogame <ken.tsuki@hotmail.com>
2018-03-29 14:28:40 +0700

    migrate question model

--- quiz/models.py
+++ quiz/models.py
@@ -1,13 +1,12 @@
 from django.db import models
-'''
+
 # Question to ask answer only True or False
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
 
 
-# Possible anser True or False, include amount answered
+# Possible answer True or False, include amount answered
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice = models.CharField(max_length=5)
     correct_answer = models.IntegerField(default=0)
-'''
